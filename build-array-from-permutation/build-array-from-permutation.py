56a23e268c84bc254ebf1ccbfa8e8197b4606c90
Ken Hamamoto <khamamoto12@gmail.com>
2021-09-12 16:57:36 -0700

    Time: 186 ms (5.15%), Space: 13.8 MB (32.43%) - LeetHub

---
+++ build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = [0]*len(nums)
+        print(ans)
+        for i in range(len(nums)):
+            ans[i] = nums[nums[i]]
+        return ans
